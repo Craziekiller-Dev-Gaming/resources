d71fbbdb5a6c3b1c39552aa4a6d1a67de783595e
Craziekiller_Dev_Gaming <74330846+Craziekiller-Dev-Gaming@users.noreply.github.com>
2025-08-17 13:34:35 -0400

    Initial 14

---
+++ [standalone]/[VandB]/[vehicles]/EngineSounds/fxmanifest.lua
@@ -0,0 +1,87 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gp1wb_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gp1wb_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gp1wb'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/jestermk4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/jestermk4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_jestermk4'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/elegyrh7_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/elegyrh7_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rh7elegy'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sunrise_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sunrise_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sunrise'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/severo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/severo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_severo'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/asteropers_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/asteropers_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_asteropers'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/coquette4c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/coquette4c_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_coquette4c'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/jugularest_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/jugularest_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_jugularest'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/roxanne_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/roxanne_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_roxanne'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sultanrsv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sultanrsv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v8sultanrs'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mf1_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mf1_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_progenmf1'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nsandstorm_game.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sheavas_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sheavas_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sheavas'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg247hemi57_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg247hemi57_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg247hemi57'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg18bmwm4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg18bmwm4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg18bmwm4'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tgrcara_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tgrcara_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tgrcara'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gbbisonhf_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gbbisonhf_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gbbisonhf'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gbstarlight_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gbstarlight_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gbstarlight'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gbbisonstx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gbbisonstx_game.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gbbisonstx'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gbsolacev_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gbsolacev_game.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gbsolacev'
