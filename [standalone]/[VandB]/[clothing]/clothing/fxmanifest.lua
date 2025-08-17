a3a2202d100850714261fb270d375fbd9bb1cfda
Craziekiller_Dev_Gaming <74330846+Craziekiller-Dev-Gaming@users.noreply.github.com>
2025-08-17 13:16:44 -0400

    Initial 13

---
+++ [standalone]/[VandB]/[clothing]/clothing/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_bbc.meta',
+  'mp_f_freemode_01_bbc.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_bbc.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_bbc.meta'
+
