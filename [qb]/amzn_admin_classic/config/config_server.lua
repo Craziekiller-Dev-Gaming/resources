2dfdda8bac936ea0ef1ef6536a000ef3bc902b3e
Craziekiller_Dev_Gaming <74330846+Craziekiller-Dev-Gaming@users.noreply.github.com>
2025-08-24 19:17:36 -0400

    Admin Menu update

---
+++ [qb]/amzn_admin_classic/config/config_server.lua
@@ -0,0 +1,9 @@
+Config                                      = Config or {}  -- Don't touch!
+
+-- Logs/Screenshot feature Config
+-- Set the discord webhook for the logs and screenshots.
+Config.LogsWebhook                          = "https://discord.com/api/webhooks/1409314331115520010/arNvPr8Lsl7vXB0A8M06oQJ9exv6HP-vWyyhDKUPiveeSt-pU0QRgZrXseoAj6VpeRDX"
+Config.ScreenshotWebhook                    = "https://discord.com/api/webhooks/1409314331115520010/arNvPr8Lsl7vXB0A8M06oQJ9exv6HP-vWyyhDKUPiveeSt-pU0QRgZrXseoAj6VpeRDX"
+
+-- Set this to your discord bot token for the discord roles integration. This is not required for anything else.
+Config.DiscordRolesBotToken = ""
